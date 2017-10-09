57506b139dd1e16ecd5840621c1a80448324821e
Alexander Voyuev <sashafox13@gmail.com>
2017-10-09 12:02:15 +0300

    BugFix

--- src/main/java/com/step/config/MongoConfig.java
+++ src/main/java/com/step/config/MongoConfig.java
@@ -4,7 +4,9 @@ import com.mongodb.MongoClient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.net.UnknownHostException;
